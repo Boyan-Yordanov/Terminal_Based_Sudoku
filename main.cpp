8e440815e24c28e5bc22db2cddc27235529c1995
Boyan-Yordanov <57684975+Boyan-Yordanov@users.noreply.github.com>
2021-06-24 10:33:22 +0100

    My final submission
    This is part of the code that I submitted for the project.

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include  "Sudoku.hpp"
+
+int main()
+{
+/////////////////////////////////////////
+/// Initialising a object to call the ///
+/// Function that runs the mini game  ///
+/////////////////////////////////////////
+   SudokuMinigame a;
+   a.kindaMainFunctionButNotQuite(); 
+}
